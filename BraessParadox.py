53094a0d1d64947a78a9e414996fdbe72d607a5b
Elan <elanbmiller@gmail.com>
2018-05-06 15:27:24 -0500

    finishing fict play w no bridge

--- BraessParadox.py
+++ BraessParadox.py
@@ -21,25 +21,75 @@
 #   Observe the reward X_{j,t} and update the empirical mean for the chosen action.
 # 	https://jeremykun.com/2013/10/28/optimism-in-the-face-of-uncertainty-the-ucb1-algorithm/
 
+import networkx as nx
+from random import randint
+import random
+import matplotlib.pyplot as plt
+
 class car:
 	
-	def _init_(self, identity):
-		this.identity = identity
+	def __init__(self, identity):
+		self.identity = identity
 		#Keep track of first and second (and possibly third) choice on path
-		this.path = []
+		self.first = ' '
+		self.second = ' '
+		self.third = ' '
 		#Keep track of choices in the past and times perhaps
-		this.historicalChoices = {}
+		self.historicalChoices = {}
 	
 	#Pick path based on some algorithm and the location you're at and historical travel
 	#Also, update historical choices in here
-	def choosePath(self, currLocation, algo):
-		
-class path:
 	
-	def _init(self, name, cost):
-		this.name = name
-		this.cost = cost
-		
+def createCars(numCars, location):
+	cars = []
+	for c in range(numCars):
+		newCar = car(c)
+		newCar.first = location
+		cars.append(newCar)
+	return cars
+
+def fictPlayNoBridge(numberOfCars):
 	
-		
-		
+	cars = createCars(numberOfCars, 'S')
+
+	#Case of no highway, fictitious play
+	#Assume other players chose randomly, so should you!!
+	timeSum = 0.0
+	for i in range(10):
+		carsNorth = 0
+		carsSouth = 0
+		for car in cars:
+			goNorth = random.randint(0, 1)
+			if goNorth:
+				car.second = 'A'
+				carsNorth += 1
+			else:
+				car.second = 'B'
+				carsSouth +=1
+
+		northCarCost = (float(carsNorth/100.0) + 45.0)*float(carsNorth)
+		southCarCost = (float(carsSouth/100.0) + 45.0)*float(carsSouth)
+		avgCost = float(northCarCost + southCarCost)/numberOfCars
+		timeSum += avgCost
+	return(timeSum/10.0) #average time is 65.0021285 in 10 iterations
+	
+def fictPlay_W_Bridge(numberOfCars):
+	
+
+if __name__ == '__main__':
+	numCars = 4000
+	X = []
+	Y = []
+	for i in range(10):
+		Y.append(fictPlayNoBridge(numCars))
+		X.append(numCars)
+		numCars+=1000
+	plt.title("Average time per car (no highway) ")
+	plt.plot(X,Y)
+	plt.show()
+
+#Case of highway, fictitious play
+#Assume other players pick logically
+	
+	
+	
